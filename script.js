2d267970934d5c77220f8b574aa4fc7ef1875c67
Rahulsunda1 <rahulsunda977@gmail.com>
2024-10-22 17:41:16 +0530

    added correct path

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let getMovie = () => {
                           <div>
                               <h2>${data.Title}</h2>
                               <div class="rating">
-                                  <img src="star.svg">
+                                  <img src="./star.svg">
                                   <h4>${data.imdbRating}</h4>
                               </div>
                               <div class="details">
